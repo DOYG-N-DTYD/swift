d128b536da001702eedc51f238d703172642831d
DOYG-N-DTYD <112571712+DOYG-N-DTYD@users.noreply.github.com>
2024-12-26 13:04:14 +0000

    coreBasics.swift very begin ended

---
+++ learnHard/coding/coreBasics.swift
@@ -0,0 +1,45 @@
+// The Swift Programming Language
+import Foundation
+
+// shift + alt + a comment multiple lines
+
+/* var dataDouble : Double = 3.33
+print(dataDouble)
+dataDouble.round()
+print(Int(dataDouble))
+
+var a: Int8 = 10
+var b: Int8 = 10
+var c: Int32 = Int32(a+b)
+print(c) */
+
+/* var dataDouble : Double = 3.1
+var dataInt : Int32 = 10
+print(Double(dataInt) / dataDouble) */
+
+/* var stringData : Double 
+stringData = Double("3.14") ?? 0
+print(stringData) */
+/* 
+var tupleName1 = (true, "string", 88) // неявно
+var tupleName2 : (Bool, String, Int) = (true, "string", 88) // явно
+ */
+
+/* var userInfo: (boolVal:Bool, Int, String) = (true, 34, "Tom")
+let(_, age, name) = userInfo
+print(userInfo.boolVal) */
+
+/* if(true){
+
+} else if (false) {
+
+} else {
+    
+} */
+
+/* print(2 > 3 ? 6 : 7) // тернарный оператор
+if( 2 > 3 ) {
+    print(6)
+}else {
+    print(7)
+} */
